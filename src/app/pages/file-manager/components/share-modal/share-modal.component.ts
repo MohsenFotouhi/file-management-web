2c01088be22e4a333e4f17c5f726b5eefba4df1a
Farhang Darzi <farhang.darzi@gmail.com>
2024-10-01 13:17:34 +0330

    fix email list to api

--- src/app/pages/file-manager/components/share-modal/share-modal.component.ts
+++ src/app/pages/file-manager/components/share-modal/share-modal.component.ts
@@ -1,21 +1,19 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogContent, MatDialogTitle } from "@angular/material/dialog";
-import { FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { SearchService } from "../../../../services/search.service";
-import { Subscription } from "rxjs";
-import { SearchUser } from "../../../../interface/search-interface";
-import { MatIconModule } from "@angular/material/icon";
+import { Component, Inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
-import { PersianCalendarComponent } from '../persian-calendar/persian-calendar.component';
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MAT_DIALOG_DATA, MatDialog, MatDialogContent, MatDialogTitle } from "@angular/material/dialog";
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { ShareService } from 'src/app/services/share.service';
-import { User } from 'src/app/interface/shared-file';
-import { CreateDownloadLinkCommand } from 'src/app/interface/share-models';
-import { FileManagerService } from 'src/app/services/file-manager.service';
+import { MatIconModule } from "@angular/material/icon";
 import * as moment from 'jalali-moment';
-import { DateTime } from 'luxon';
+import { Subscription } from "rxjs";
+import { CreateDownloadLinkCommand } from 'src/app/interface/share-models';
+import { User } from 'src/app/interface/shared-file';
+import { ShareService } from 'src/app/services/share.service';
+import { SearchUser } from "../../../../interface/search-interface";
+import { SearchService } from "../../../../services/search.service";
+import { PersianCalendarComponent } from '../persian-calendar/persian-calendar.component';
 
 @Component({
   selector: 'vex-share-modal',
@@ -34,7 +32,8 @@ import { DateTime } from 'luxon';
   templateUrl: './share-modal.component.html',
   styleUrl: './share-modal.component.scss'
 })
-export class ShareModalComponent {
+export class ShareModalComponent
+{
 
   selectedDate!: Date;
   isPublic = new FormControl(false);
@@ -54,20 +53,24 @@ export class ShareModalComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
     private dialog: MatDialog,
     private searchService: SearchService,
-    private shareService: ShareService) {
-    this.virtualPath = data.VirtualPath
+    private shareService: ShareService)
+  {
+    this.virtualPath = data.VirtualPath;
   }
 
-  openCalendar(): void {
+  openCalendar(): void
+  {
     const dialogRef = this.dialog.open(PersianCalendarComponent, {
       width: '300px',
       height: '320px',
       data: { selectedDate: this.selectedDate }
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(result)
-      if (result) {
+    dialogRef.afterClosed().subscribe(result =>
+    {
+      console.log(result);
+      if (result)
+      {
         this.selectedDate = result;
         this.expireDate.setValue(result);
       }
@@ -75,50 +78,63 @@ export class ShareModalComponent {
   }
 
 
-  onSearch(): void {
-    if (this.searchValue.value) {
+  onSearch(): void
+  {
+    if (this.searchValue.value)
+    {
       if (this.searchSub) this.searchSub.unsubscribe();
       this.searchLoading = true;
-      this.searchSub = this.searchService.search(this.searchValue.value || '').subscribe(res => {
+      this.searchSub = this.searchService.search(this.searchValue.value || '').subscribe(res =>
+      {
         this.searchLoading = false;
         this.userList = res;
-      }, () => {
+      }, () =>
+      {
         this.searchLoading = false;
         this.userList = [];
-      })
+      });
     }
   }
 
-  onSelectUser(user: SearchUser): void {
+  onSelectUser(user: SearchUser): void
+  {
     const index = this.selectedUserList?.findIndex(i => i.userGUID === user.userGUID);
-    if (index > -1) {
+    if (index > -1)
+    {
       this.selectedUserList.splice(index, 1);
-    } else {
-      this.selectedUserList.push(user)
+    } else
+    {
+      this.selectedUserList.push(user);
     }
   }
 
-  isUserSelected(user: SearchUser): boolean {
+  isUserSelected(user: SearchUser): boolean
+  {
     return this.selectedUserList.findIndex(i => i.userGUID === user.userGUID) > -1;
   }
 
-  addEmail(): void {
-    if (this.emailValue.valid && this.emailValue.value) {
-      const email: any = this.emailValue.value
+  addEmail(): void
+  {
+    if (this.emailValue.valid && this.emailValue.value)
+    {
+      const email: any = this.emailValue.value;
       this.emailList.push(email);
       this.emailValue.setValue('');
     }
   }
 
-  removeEmail(i: number): void {
+  removeEmail(i: number): void
+  {
     this.emailList.splice(i, 1);
   }
 
-  closeModal(): void {
+  closeModal(): void
+  {
     this.dialog.closeAll();
   }
 
-  createLink(): void {
+  createLink(): void
+  {
     const selectedUser: User[] = [];
     const selectedEmails: string[] = [];
     this.selectedUserList.forEach(x => selectedUser.push({ userId: x.userGUID, username: x.username }));
@@ -127,28 +143,31 @@ export class ShareModalComponent {
 
     const jalaliDate = this.selectedDate; // تاریخ شمسی به فرمت YYYY/MM/DD
     const gregorianDate = moment(jalaliDate, 'jYYYY/jMM/jDD').toDate();
-    
+
     // فرمت خروجی به صورت YYYY-MM-DD
 
+    console.log('emailList', this.emailList);
     const shareObject: CreateDownloadLinkCommand = {
       isPermanent: this.isPermanent.value !== null ? this.isPermanent.value : false,
       isPublic: this.isPublic.value !== null ? this.isPublic.value : false,
-      expireDateTime: gregorianDate ,
+      expireDateTime: gregorianDate,
       virtualPath: this.virtualPath,
       shareWithUsers: selectedUser,
-      shareWithEmails: selectedEmails,
+      shareWithEmails: this.emailList,
     };
 
-    this.shareService.createDownloadLink(shareObject) .subscribe({
+    this.shareService.createDownloadLink(shareObject).subscribe({
       next: (res) => console.log('Success', res),
-      error: (error) => {
+      error: (error) =>
+      {
         console.error('There was an error:', error);
-        if (error.error.errors) {
+        if (error.error.errors)
+        {
           console.log('Validation Errors:', error.error.errors);
         }
       }
     });
-  
+
   }
 
 
