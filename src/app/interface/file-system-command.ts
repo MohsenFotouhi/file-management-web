7524e6281426e11d7711e584679faf235fda0a44
ellahe <ellaheaziziyan@gmail.com>
2024-10-02 15:03:50 +0330

    fix upload

---
+++ src/app/interface/file-system-command.ts
@@ -0,0 +1,12 @@
+export enum FileSystemCommand {
+    GetDirContents = "GetDirContents",
+    CreateDir = "CreateDir",
+    Rename = "Rename",
+    Move = "Move",
+    Copy = "Copy",
+    Remove = "Remove",
+    UploadChunk = "UploadChunk",
+    AbortUpload = "AbortUpload",
+    Download = "Download",
+    DownloadChunk = "DownloadChunk",
+}
